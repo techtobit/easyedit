c26989af0aceb263d39ca7529481c972bef65a21
ashraf <nurthern2016@gmail.com>
2026-01-28 02:04:51 +0600

    future: added neonDB, track of proccessing request

---
+++ core/database.py
@@ -0,0 +1,28 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,     
+    pool_size=5,
+    max_overflow=10,
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
